bbbae9c6c24bef9adcd53ba52b04879569dd56a3
Neal Peters <nealcpeters@gmail.com>
2014-05-20 11:15:57 -0500

    added store controller, bike object, and html display of object via controller

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-var app = angular.module('store', []);
+(function() {
+  var bike = {manufacturer: "Surly", model: "Long Haul Trucker", price: "$1,000"}
+  var app = angular.module('store', []);
+  app.controller('StoreController', function(){
+    this.product = bike;});
+})();
+
